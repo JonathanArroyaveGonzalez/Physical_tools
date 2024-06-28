97e40f4a0bab6a4073f9243f2fb6f0823fdc8aae
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2024-06-27 19:31:23 -0500

    feat(master):addition percentage error calculator

--- Inicio.py
+++ Inicio.py
@@ -1,20 +1,27 @@
 import streamlit as st
-from layout import regressions, home, ley_de_hooke
+from layout import regressions, home, ley_de_hooke, calculator_mod
 from styles.theme import theme
 
 PAGES = {
-    "Inicio": home,
-    "Calculadora de Regresiones": regressions,
-    "Simulador Ley de Hooke": ley_de_hooke,
+    "🚩 Inicio": home,
+    "🚩 Calculadora de Regresiones": regressions,
+    "🚩 Simulador Ley de Hooke": ley_de_hooke,
+    "🚩 Calculadora de % de error": calculator_mod,
 }
 
 
 def main():
+    st.set_page_config(
+        page_title="Physical Tools",
+        page_icon=":cyclone:",
+        layout="wide",
+    )
     with st.sidebar:
         theme()
 
-    st.sidebar.title("Navegación")
-    choice = st.sidebar.radio("Ir a", list(PAGES.keys()))
+    st.sidebar.title("➤ Navegación")
+    choice = st.sidebar.radio("Ir a 📌", list(PAGES.keys()))
+    st.sidebar.header("Acerca de ...")
 
     PAGES[choice].write()  # Simplified page rendering
 
